9b7fe0756939768a6b81f65bcf76fad206a2b7ad
Dilan Dev <dilan.developer22@gmail.com>
2024-08-12 03:09:49 -0500

    refactor: add the all routes that using for project

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import PhotoPage from "./pages/PhotoPage/PhotoPage";
+import { createContext, useState } from "react";
+
+const PhotoFavoriteContext = createContext();
+
 function App() {
+  const [numberPhotosFavorites] = useState(0);
+
 
   return (
-    <>
-    </>
-  )
+    <PhotoFavoriteContext.Provider value={numberPhotosFavorites}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/photos" element={<div>Welcome to photos pages</div>} />
+          <Route path="/photos/:photo_id" element={<PhotoPage />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </Router>
+    </PhotoFavoriteContext.Provider>
+  );
 }
 
-export default App
+export { PhotoFavoriteContext };
+export default App;
