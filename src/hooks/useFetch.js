65fea84da4a0c4ba203bf6e33833ec258bfe002f
Dilan Dev <dilan.developer22@gmail.com>
2024-08-12 03:07:00 -0500

    feat: add custom hook that call api for get a type of photos

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+
+const api_key = import.meta.env.VITE_API_KEY;
+const url_base = import.meta.env.VITE_URL_BASE;
+
+export const useFetch = (query) => {
+	const [data, setData] = useState([]);
+	// const [error, setError] = useState(null);
+	// const [loading, setLoading] = useState(true);
+	const getImages = async () => {
+		try {
+			let response = await fetch(`${url_base}/search?query=${query}`, {
+				headers: {
+					Authorization: api_key,
+				},
+			});
+			let result = await response.json();
+			setData(result.photos);
+			// setLoading(false);
+		} catch (error) {
+			// setLoading(false);
+			// setError("hubo un error al llamar la api");
+			console.log(error);
+		}
+	};
+
+	useEffect(() => getImages, []);
+
+	// return { data, loading, error };
+	return { data };
+};
