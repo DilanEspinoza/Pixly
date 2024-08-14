c09703b5b54cdff890cd1dc82810be308dc4f787
Dilan Dev <dilan.developer22@gmail.com>
2024-08-14 03:37:10 -0500

    fix: add getData function inside useEffect

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -7,24 +7,26 @@ export const useFetch = (query) => {
 	const [data, setData] = useState([]);
 	// const [error, setError] = useState(null);
 	// const [loading, setLoading] = useState(true);
-	const getImages = async () => {
-		try {
-			let response = await fetch(`${url_base}/search?query=${query}`, {
-				headers: {
-					Authorization: api_key,
-				},
-			});
-			let result = await response.json();
-			setData(result.photos);
-			// setLoading(false);
-		} catch (error) {
-			// setLoading(false);
-			// setError("hubo un error al llamar la api");
-			console.log(error);
-		}
-	};
 
-	useEffect(() => getImages, []);
+	useEffect(() => {
+		const getImages = async () => {
+			try {
+				let response = await fetch(`${url_base}/search?query=${query}`, {
+					headers: {
+						Authorization: api_key,
+					},
+				});
+				let result = await response.json();
+				setData(result.photos);
+				// setLoading(false);
+			} catch (error) {
+				// setLoading(false);
+				// setError("hubo un error al llamar la api");
+				console.log(error);
+			}
+		};
+		getImages();
+	}, [query]);
 
 	// return { data, loading, error };
 	return { data };
